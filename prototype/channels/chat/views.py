a1a505e943aa1d11170dcc84f5c5dc14c0286284
bill <bill@naver.com>
2019-12-21 00:59:09 +0900

    add: channels 를 이용한 샘플 prototype 폴더에 추가하고 패키지를 최신으로 설치해라 index.html에 사용자이름을 출력하고 사용자를 클릭했을때 이름으로 채팅방을 만들어라

---
+++ prototype/channels/chat/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
+from django.contrib.auth.models import User
+
+def index(request):
+    users = User.objects.all()
+    return render(request, 'chat/index.html', {'users':users})
+
+@login_required
+def room(request, username):
+    return render(request, 'chat/room.html', {
+        'room_name_json': mark_safe(json.dumps(username)),
+        'username': mark_safe(json.dumps(request.user.username)),
+    })
